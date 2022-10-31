1a45c8d4bc190d1648c6f01763bf8e349d7d2147
Iaroslav Uliantcev <doudou239@gmail.com>
2022-10-31 22:07:46 +0300

    First testing of react js app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+
+
+function showResult(x, y){
+    return x+y
+}
 
 function App() {
+    const [a, setA] = useState(0)
+    const [b, setB] = useState(0)
+    const [res, setRes] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>First unit tests with js</h1>
+        <input
+            type={"number"}
+            placeholder={"A"}
+            onChange={(e)=>{setA(Number(e.target.value))}}
+        />
+        <input
+            type={"number"}
+            placeholder={"B"}
+            onChange={(e)=>{setB(Number(e.target.value))}}
+        />
+        <button
+            onClick={()=>{setRes(showResult(a,b))}}
+        >=</button>
+        <p>{res}</p>
     </div>
   );
 }
 
+module.exports = {showResult}
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const {showResult} = require('./App')
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('sum should return the sum of two numbers', ()=>{
+  expect(showResult(1,3)).toBe(4)
+})
