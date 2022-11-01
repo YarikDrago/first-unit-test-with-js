1ee2d86d1a9367ae7343096c98573260a487fba2
Iaroslav Uliantcev <doudou239@gmail.com>
2022-11-01 22:28:31 +0300

    Add new tests

--- src/App.js
+++ src/App.js
@@ -5,11 +5,38 @@ function showResult(x, y){
     return x+y
 }
 
+function workWithNull(){
+    return null
+}
+
+function workWithArray(arr){
+    return arr
+}
+
+function filterArray(arr){
+    return arr.filter(val => !!val)
+}
+
+function groupBy(arr, prop){
+    console.log(typeof prop)
+   return arr.reduce((acc, index)=> {
+       const key= typeof prop === 'function' ? prop(index) : index[prop]
+       // const key= prop(index)
+       if (!acc[key]){
+           acc[key] = []
+       }
+       acc[key].push(index)
+       return acc
+   }, {})
+}
+
 function App() {
     const [a, setA] = useState(0)
     const [b, setB] = useState(0)
     const [res, setRes] = useState(0)
-
+    const testArr = [2.1, 2.2, 4.9, 5.3]
+    console.log(groupBy(testArr, Math.floor))
+    console.log("three"["length"])
   return (
     <div className="App">
         <h1>First unit tests with js</h1>
@@ -31,5 +58,8 @@ function App() {
   );
 }
 
-module.exports = {showResult}
+export {App ,showResult, workWithNull, workWithArray, filterArray, groupBy}
+// module.exports = {showResult, workWithNull, workWithArray, filterArray}
+// export {showResult}
+// export default
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-const {showResult} = require('./App')
+const {showResult, workWithNull, workWithArray, filterArray, groupBy} = require('./App')
 
 // test('renders learn react link', () => {
 //   render(<App />);
@@ -9,6 +9,69 @@ const {showResult} = require('./App')
 //   expect(linkElement).toBeInTheDocument();
 // });
 
-test('sum should return the sum of two numbers', ()=>{
-  expect(showResult(1,3)).toBe(4)
+describe('Sum funciton', ()=>{
+  test('sum should return the sum of two numbers', ()=>{
+    expect(showResult(1,3)).toBe(4)
+    expect(showResult(1,3)).toEqual(4)
+    expect(showResult(3,4)).toBeGreaterThan(6)
+  })
+  test("test sum of two float numbers", ()=>{
+    expect(showResult(0.1,0.2)).toBeCloseTo(0.3)
+  })
 })
+
+describe('work with null', ()=>{
+  test('tests', ()=>{
+    expect(workWithNull()).toBe(null)
+    expect(workWithNull()).toBeNull()
+    expect(workWithNull()).toBeFalsy()
+    expect(workWithNull()).toBeDefined()
+    expect(workWithNull()).not.toBeUndefined()
+    expect(workWithNull()).not.toBeTruthy()
+  })
+})
+
+describe('work with arrays', ()=>{
+  let arr
+  beforeEach(()=>{
+    arr = [1, null, "1", 0, undefined, 0.1, true, false]
+  })
+  test('return same array', ()=>{
+    // const arr = [1, null, "1", 0, undefined, 0.1, true, false]
+    expect(workWithArray(arr)).toBe(arr)
+  })
+  test('filter array from false values', ()=>{
+    // const arr = [1, null, "1", 0, undefined, 0.1, "", true, false]
+    const arrResult = [1, "1", 0.1, true]
+    expect(filterArray(arr)).toEqual(arrResult)
+  })
+  test('test array with NOT contain elements', ()=>{
+    // const arr = [1, null, "1", 0, undefined, 0.1, "", true, false]
+    const arrRes = [null, undefined, false, 0, ""]
+    expect(filterArray(arr)).not.toContain(arrRes)
+  })
+})
+
+describe("groupBy tests", ()=>{
+  test("Math.floor",()=>{
+    const arr = [2.1, 2.2, 4.9, 5.0]
+    const res = {
+      2: [2.1, 2.2],
+      4: [4.9],
+      5: [5.0]
+    }
+    expect(groupBy(arr, Math.floor)).toEqual(res)
+
+  })
+  test('string length', ()=>{
+    const arr = ["one", "two", "three"]
+    const res = {
+      3: ["one", "two"],
+      5: ["three"]
+    }
+    expect(groupBy(arr, "length")).toEqual(res)
+
+  })
+})
+
+
